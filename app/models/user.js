ef20bae944dc0911a77914fb4724d11ce1d63585
bhumesh <bhumesh@localhost.localdomain>
2020-05-19 15:54:26 +0530

    database schema

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,7 @@ var UserSchema = new Schema({
 	name: { type: String, lowercase: true, required: true, unique: false },
 	email: { type: String, lowercase: true, required: true, unique: false },
 	mobile: { type: String, lowercase: true, required: true, unique: false },
-	rating: { type: Number, required: true, unique: false },
+	rating: { type: String, lowercase: true, required: true, unique: false },
 	review: { type: String, lowercase: true, required: true, unique: false }
 });
 
