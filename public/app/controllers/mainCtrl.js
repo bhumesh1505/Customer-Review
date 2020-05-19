6696f09a2dca547165294b98cf1ae8107cfd9792
bhumesh <bhumesh@localhost.localdomain>
2020-05-19 20:01:44 +0530

    write angular controllers

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,4 +1,34 @@
 angular.module('mainController',[])
-.controller('mainCtrl',function($scope,$location,$timeout,$rootScope){
-        $scope.userdetails = {};
+.controller('mainCtrl',function($scope,$location,$timeout,$rootScope,User){
+        $scope.successMsg = false;
+        $scope.errorMsg = false;
+        $scope.maxratings = [1,2,3,4,5];
+        $scope.selectedRating = 0;
+
+        $scope.submitReview = function(userData){
+            $scope.successMsg = false;
+            $scope.errorMsg = false;
+            if(userData){
+                userData.rating = $scope.selectedRating;
+            }
+            console.log(userData);
+            var successcallbackadd = function(data){
+                console.log(data);
+                if (data.success) {
+                    $scope.successMsg = data.msg;
+                    $timeout(function(){
+                        $location.path('/showallreviews');
+                    },1000);
+                }
+                else {
+                    $scope.errorMsg = data.msg;
+                }
+            };
+            var errorcallback = function(data){
+            };
+            User.addReview(userData,successcallbackadd,errorcallback);
+        }
+        $scope.setRating = function(x){
+            $scope.selectedRating = x;
+        }
 });
